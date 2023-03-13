788f8210c87074ef6ba7374e44be604a80ca12ed
Andriy Plish <113835398+Andriy723@users.noreply.github.com>
2023-03-13 14:31:26 +0200

    Changing the lab

--- src/ua/lviv/iot/algo/part1/lab1/Trolleybus.java
+++ src/ua/lviv/iot/algo/part1/lab1/Trolleybus.java
@@ -1,38 +1,42 @@
 package ua.lviv.iot.algo.part1.lab1;
 import lombok.*;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-
 public class Trolleybus {
-    private static int id = 100;
+    private int id = 100;
     private int routeNumber;
     private int currentStop;
     private double maxSpeed;
     private int passengers;
+    private int currentSpeed;
     private static Trolleybus defaultTrolleybus = new Trolleybus();
 
     public static Trolleybus getInstance() {
         return defaultTrolleybus;
     }
-
     public double stop() {
-        return maxSpeed * 0;
+        if (currentSpeed < maxSpeed) {
+            return currentSpeed = 0;
+        }
+        else {
+            System.out.println("Error");
+        }
+        return currentSpeed;
     }
 
     public double start() {
-        return maxSpeed = 20;
+        return currentSpeed = 20;
     }
 
     public double addPassenger() {
 
-        for (int i = 0; i < passengers; i++) {
+        for (int i = 0; i < 25; i++) {
             passengers++;
         }
-        for (int i = 0; i >= passengers; i++) {
+        for (int i = 0; i >= 25; i++) {
             System.out.println("  The trolley bus is crowded \n");
             break;
         }
@@ -41,46 +45,24 @@ public class Trolleybus {
 
     public double removePassenger() {
 
-        if (passengers > 0) {
+        if (passengers >= 25) {
             passengers--;
-        } else if (passengers == 0) {
-            System.out.println("  The trolleybus is not crowded \n");
-        } else {
-            System.out.println("  Error! \n");
         }
-
-        return 0;
+        return passengers;
     }
 
     public static void main(String[] args) {
 
-        Trolleybus[] object = new Trolleybus[4];
-        object[0] = new Trolleybus();
-        object[1] = new Trolleybus(100, 50, 40, 45);
-        object[2] = getInstance();
-        object[3] = getInstance();
+        Trolleybus[] objects = {
+            new Trolleybus(),
+            new Trolleybus(100,50, 40 ,50,4,0),
+            Trolleybus.getInstance(),
+            Trolleybus.getInstance()
+        };
 
-        object[1].stop();
-        object[0].start();
-        object[2].addPassenger();
-        object[1].removePassenger();
-
-        System.out.println("The trolley 1:\t");
-        System.out.println(object[0]);
-
-        System.out.println("\nThe trolley 2:");
-        System.out.println(object[1]);
-
-        System.out.println("\nThe trolley 3:");
-        System.out.println(object[2]);
-
-        System.out.println("\nThe trolley 4:");
-        System.out.println(object[3]);
-
-        System.out.println("\n\n");
-
-        for (int k = 0; k < object.length; k++) {
-            System.out.println(object.toString());
+        for (Trolleybus trolleybus: objects){
+            System.out.println(trolleybus.toString());
         }
+
     }
 }
